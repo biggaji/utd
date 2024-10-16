6c2237633941dbcd60e3992a83698865443dc2bc
biggaji <tobiajibade2017@yandex.com>
2024-10-16 11:06:47 +0100

    feat: replace fetchAllusers func with ESM with keyword

--- server.ts
+++ server.ts
@@ -1,3 +1,6 @@
+import 'jsr:@std/dotenv/load';
+
+const PLATFORM_NAME = Deno.env.get('APP_NAME');
 Deno.serve({ port: 4343 }, (_req) => {
-	return new Response('Hello world from Deno APIs');
+	return new Response(`Hello world from ${PLATFORM_NAME}`);
 });
